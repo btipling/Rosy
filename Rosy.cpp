5c434dbb2ec376f92314c67307dadd5d448eb065
btipling <bjorn@ambientchill.com>
2024-11-11 16:54:32 -0800

    Add project files.

---
+++ Rosy.cpp
@@ -0,0 +1,60 @@
+// Rosy.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "Rosy.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    const int WIDTH = 640;
+    const int HEIGHT = 480;
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("Hello SDL", WIDTH, HEIGHT, SDL_WINDOW_VULKAN);
+    renderer = SDL_CreateRenderer(window, NULL);
+
+    uint32_t extensionCount;
+    auto extensionNames = SDL_Vulkan_GetInstanceExtensions(&extensionCount);
+
+
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Rosy";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledLayerCount = 0;
+    createInfo.ppEnabledLayerNames = NULL;
+    createInfo.enabledExtensionCount = extensionCount;
+    createInfo.ppEnabledExtensionNames = extensionNames;
+
+    VkInstance instance;
+    VkResult result = vkCreateInstance(&createInfo, NULL, &instance);
+
+    if (result == VK_SUCCESS) {
+        std::cout << "Vulkan instance created successfully!" << std::endl;
+
+        // Clean up Vulkan
+        vkDestroyInstance(instance, NULL);
+    }
+    else {
+        std::cout << "Failed to create Vulkan instance! Error code: " << result << std::endl;
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
