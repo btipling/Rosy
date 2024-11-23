7d0fc54f5d7a9db274f78c1a1cf5c171a2f38777
btipling <bjorn@ambientchill.com>
2024-11-22 20:50:01 -0800

    do the staging buffer thing

--- src/rhi/rhi_buffer.cpp
+++ src/rhi/rhi_buffer.cpp
@@ -6,7 +6,9 @@ GPUMeshBuffersResult Rhi::uploadMesh(std::span<uint32_t> indices, std::span<Vert
 	const size_t vertexBufferSize = vertices.size() * sizeof(Vertex);
 	const size_t indexBufferSize = indices.size() * sizeof(uint32_t);
 
-	auto bufferResult = createBuffer(vertexBufferSize, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
+	auto bufferResult = createBuffer(
+		vertexBufferSize, 
+		VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
 		VMA_MEMORY_USAGE_GPU_ONLY);
 	if (bufferResult.result != VK_SUCCESS) {
 		rv.result = bufferResult.result;
@@ -20,7 +22,9 @@ GPUMeshBuffersResult Rhi::uploadMesh(std::span<uint32_t> indices, std::span<Vert
 
 	rv.buffers.vertexBufferAddress = vkGetBufferDeviceAddress(m_device.value(), &deviceAddressInfo);
 
-	bufferResult = createBuffer(indexBufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+	bufferResult = createBuffer(
+		indexBufferSize, 
+		VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
 		VMA_MEMORY_USAGE_GPU_ONLY);
 	if (bufferResult.result != VK_SUCCESS) {
 		rv.result = bufferResult.result;
@@ -28,6 +32,41 @@ GPUMeshBuffersResult Rhi::uploadMesh(std::span<uint32_t> indices, std::span<Vert
 	}
 
 	rv.buffers.indexBuffer = bufferResult.buffer;
+
+	bufferResult = createBuffer(vertexBufferSize + indexBufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_CPU_ONLY);
+	if (bufferResult.result != VK_SUCCESS) {
+		rv.result = bufferResult.result;
+		return rv;
+	}
+	AllocatedBuffer staging = bufferResult.buffer;
+
+	void* data;
+	vmaMapMemory(m_allocator.value(), staging.allocation, &data);
+	memcpy(data, vertices.data(), vertexBufferSize);
+	memcpy((char*)data + vertexBufferSize, indices.data(), indexBufferSize);
+
+	VkResult result;
+	result = immediateSubmit([&](VkCommandBuffer cmd) {
+		VkBufferCopy vertexCopy{ 0 };
+		vertexCopy.dstOffset = 0;
+		vertexCopy.srcOffset = 0;
+		vertexCopy.size = vertexBufferSize;
+
+		vkCmdCopyBuffer(cmd, staging.buffer, rv.buffers.vertexBuffer.buffer, 1, &vertexCopy);
+
+		VkBufferCopy indexCopy{ 0 };
+		indexCopy.dstOffset = 0;
+		indexCopy.srcOffset = vertexBufferSize;
+		indexCopy.size = indexBufferSize;
+
+		vkCmdCopyBuffer(cmd, staging.buffer, rv.buffers.indexBuffer.buffer, 1, &indexCopy);
+	});
+	if (bufferResult.result != VK_SUCCESS) {
+		rv.result = bufferResult.result;
+		return rv;
+	}
+	destroyBuffer(staging);
+
 	rv.result = VK_SUCCESS;
 	return rv;
 }
