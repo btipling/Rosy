adab3b8231bfd04821b6bbbfffc6d7007a88d2c8
Bjorn Tipling <bjorn.tipling@conductorone.com>
2024-12-11 19:11:29 -0800

    define functions in cpp

--- src/app/rhi/rhi_render.cpp
+++ src/app/rhi/rhi_render.cpp
@@ -33,6 +33,74 @@ void rhi::transition_image(const VkCommandBuffer cmd, const VkImage image, const
 	vkCmdPipelineBarrier2(cmd, &dependency_info);
 }
 
+void mesh_scene::add_node(fastgltf::Node& gltf_node)
+{
+	const auto new_node = std::make_shared<mesh_node>();
+	if (gltf_node.meshIndex.has_value())
+	{
+		new_node->mesh_index = gltf_node.meshIndex.value();
+
+		auto [translation, rotation, scale] = std::get<fastgltf::TRS>(gltf_node.transform);
+
+		const auto tr = glm::vec3(translation[0], translation[1], translation[2]);
+		const auto ro = glm::quat(rotation[3], rotation[0], rotation[1], rotation[2]);
+		const auto sc = glm::vec3(scale[0], scale[1], scale[2]);
+
+		const auto tm = translate(glm::mat4(1.0f), tr);
+		const auto rm = toMat4(ro);
+		const auto sm = glm::scale(glm::mat4(1.0f), sc);
+
+		new_node->model_transform = tm * rm * sm;
+	}
+	new_node->children.reserve(gltf_node.children.size());
+	for (size_t i : gltf_node.children)	new_node->children.emplace_back(i);
+	nodes.push_back(new_node);
+};
+
+void mesh_scene::add_scene(fastgltf::Scene& gltf_scene)
+{
+	scenes.emplace_back(gltf_scene.nodeIndices.begin(), gltf_scene.nodeIndices.end());
+};
+
+[[nodiscard]] std::vector<render_object> mesh_scene::draw_queue(const size_t scene_index, const glm::mat4& m ) const
+{
+	if (scene_index >= scenes.size()) return {};
+	std::queue<std::shared_ptr<mesh_node>> queue{};
+	std::vector<render_object> draw_nodes{};
+	for (const size_t node_index : scenes[scene_index])
+	{
+		auto draw_node = nodes[node_index];
+		draw_node->update(m, nodes);
+		queue.push(draw_node);
+
+	}
+	while (queue.size() > 0)
+	{
+		const auto current_node = queue.front();
+		queue.pop();
+		if (current_node->mesh_index.has_value())
+		{
+			for (
+				const std::shared_ptr<mesh_asset> ma = meshes[current_node->mesh_index.value()];
+				const auto [start_index, count] : ma->surfaces)
+			{
+				render_object ro{};
+				ro.transform = current_node->world_transform;
+				ro.first_index = start_index;
+				ro.index_count = count;
+				ro.index_buffer = ma->mesh_buffers.index_buffer.buffer;
+				ro.vertex_buffer_address = ma->mesh_buffers.vertex_buffer_address;
+				draw_nodes.push_back(ro);
+			}
+		}
+		for (const size_t child_index : current_node->children)
+		{
+			queue.push(nodes[child_index]);
+		}
+	}
+	return draw_nodes;
+};
+
 VkResult rhi::draw_ui()
 {
 	// Draw some FPS statistics or something
--- src/app/rhi/rhi_types.h
+++ src/app/rhi/rhi_types.h
@@ -155,73 +155,9 @@ struct mesh_scene
 	std::vector<std::shared_ptr<mesh_node>> nodes;
 	std::vector<std::shared_ptr<mesh_asset>> meshes;
 
-	void add_node(fastgltf::Node& gltf_node)
-	{
-		const auto new_node = std::make_shared<mesh_node>();
-		if (gltf_node.meshIndex.has_value())
-		{
-			new_node->mesh_index = gltf_node.meshIndex.value();
-
-			auto [translation, rotation, scale] = std::get<fastgltf::TRS>(gltf_node.transform);
-
-			const auto tr = glm::vec3(translation[0], translation[1], translation[2]);
-			const auto ro = glm::quat(rotation[3], rotation[0], rotation[1], rotation[2]);
-			const auto sc = glm::vec3(scale[0], scale[1], scale[2]);
-
-			const auto tm = translate(glm::mat4(1.0f), tr);
-			const auto rm = toMat4(ro);
-			const auto sm = glm::scale(glm::mat4(1.0f), sc);
-
-			new_node->model_transform = tm * rm * sm;
-		}
-		new_node->children.reserve(gltf_node.children.size());
-		for (size_t i : gltf_node.children)	new_node->children.emplace_back(i);
-		nodes.push_back(new_node);
-	};
-
-	void add_scene(fastgltf::Scene& gltf_scene)
-	{
-		scenes.emplace_back(gltf_scene.nodeIndices.begin(), gltf_scene.nodeIndices.end());
-	};
-
-	[[nodiscard]] std::vector<render_object> draw_queue(const size_t scene_index, const glm::mat4& m = {1.f}) const
-	{
-		if (scene_index >= scenes.size()) return {};
-		std::queue<std::shared_ptr<mesh_node>> queue{};
-		std::vector<render_object> draw_nodes{};
-		for (const size_t node_index : scenes[scene_index])
-		{
-			auto draw_node = nodes[node_index];
-			draw_node->update(m, nodes);
-			queue.push(draw_node);
-				
-		}
-		while (queue.size() > 0)
-		{
-			const auto current_node = queue.front();
-			queue.pop();
-			if (current_node->mesh_index.has_value())
-			{
-				for (
-					const std::shared_ptr<mesh_asset> ma = meshes[current_node->mesh_index.value()]; 
-					const auto [start_index, count] : ma->surfaces)
-				{
-					render_object ro{};
-					ro.transform = current_node->world_transform;
-					ro.first_index = start_index;
-					ro.index_count = count;
-					ro.index_buffer = ma->mesh_buffers.index_buffer.buffer;
-					ro.vertex_buffer_address = ma->mesh_buffers.vertex_buffer_address;
-					draw_nodes.push_back(ro);
-				}
-			}
-			for (const size_t child_index: current_node->children)
-			{
-				queue.push(nodes[child_index]);
-			}
-		}
-		return draw_nodes;
-	}
+	void add_node(fastgltf::Node& gltf_node);
+	void add_scene(fastgltf::Scene& gltf_scene);
+	[[nodiscard]] std::vector<render_object> draw_queue(const size_t scene_index, const glm::mat4& m = { 1.f }) const;
 };
 
 class rhi;
@@ -230,15 +166,15 @@ class rhi_data
 {
 public:
 	explicit rhi_data(rhi* renderer);
-	std::optional<mesh_scene> load_gltf_meshes(std::filesystem::path file_path) const;
-	gpu_mesh_buffers_result upload_mesh(std::span<uint32_t> indices, std::span<vertex> vertices) const;
+	[[nodiscard]] std::optional<mesh_scene> load_gltf_meshes(std::filesystem::path file_path) const;
+	[[nodiscard]] auto upload_mesh(std::span<uint32_t> indices, std::span<vertex> vertices) const -> gpu_mesh_buffers_result;
 	allocated_buffer_result create_buffer(const char* name, const size_t alloc_size, const VkBufferUsageFlags usage, const VmaMemoryUsage memory_usage) const;
 
 
 	void destroy_buffer(const allocated_buffer& buffer) const;
 
-	allocated_image_result create_image(VkExtent3D size, VkFormat format, VkImageUsageFlags usage,
-		bool mip_mapped) const;
+	[[nodiscard]] auto create_image(VkExtent3D size, VkFormat format, VkImageUsageFlags usage,
+	                                bool mip_mapped) const -> allocated_image_result;
 	allocated_image_result create_image(const void* data, const VkExtent3D size, const VkFormat format,
 		const VkImageUsageFlags usage, const bool mip_mapped) const;
 	std::expected<ktxVulkanTexture, ktx_error_code_e> create_image(ktxTexture* ktx_texture, const VkImageUsageFlags usage) const;
