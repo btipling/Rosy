958eda341a2faf57c2e8b8c11bfecd7cf70d1bb6
Bjorn Tipling <bjorn.tipling@conductorone.com>
2025-01-05 19:56:08 -0800

    add a vcpkg script for easy install/install bug fixes

--- src/compile.ps1
+++ src/compile.ps1
@@ -2,8 +2,8 @@
 if (-not (Test-Path -Path "out")) {
     New-Item -ItemType Directory -Path "out" | Out-Null
 }
-& "slangc.exe" shaders/skybox_cube.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/skybox_cube.spv 
-& "slangc.exe" shaders/shadow.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/shadow.spv 
-& "slangc.exe" shaders/skybox.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/skybox.spv 
-& "slangc.exe" shaders/mesh.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/mesh.spv 
-& "slangc.exe" shaders/debug.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/debug.spv 
+& "slangc.exe" $PSScriptRoot/shaders/skybox_cube.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/skybox_cube.spv 
+& "slangc.exe" $PSScriptRoot/shaders/shadow.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/shadow.spv 
+& "slangc.exe" $PSScriptRoot/shaders/skybox.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/skybox.spv 
+& "slangc.exe" $PSScriptRoot/shaders/mesh.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/mesh.spv 
+& "slangc.exe" $PSScriptRoot/shaders/debug.slang -matrix-layout-column-major -profile sm_6_6 -target spirv -o out/debug.spv 
---
+++ src/install_deps.ps1
@@ -0,0 +1,20 @@
+[hashtable]$empty_dependencies = [ordered]@{
+    dependencies = @(  )
+}
+$empty_deps_string = $empty_dependencies | ConvertTo-Json
+
+[hashtable]$dependencies = [ordered]@{
+    dependencies = @(
+        'directxtex',
+        'fastgltf',
+        'stb'
+    )
+}
+$deps_string = $dependencies | ConvertTo-Json
+
+Write-Host "Removing deps"
+Set-Content -Path .\vcpkg.json -Value $empty_deps_string
+vcpkg install
+Write-Host "Reinstalling deps"
+Set-Content -Path .\vcpkg.json -Value $deps_string
+vcpkg install
