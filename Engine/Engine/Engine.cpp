ef3431b0addc24135964a5ec35091fa7c9daee8c
Bjorn Tipling <bjorn.tipling@conductorone.com>
2025-01-10 17:53:36 -0800

    Starting on an engine

--- Engine/Engine/Engine.cpp
+++ Engine/Engine/Engine.cpp
@@ -4,5 +4,38 @@
 int main(int argc, char* argv[])
 {
 	std::cout << "Hello World!\n";
+	rosy::engine engine{};
+	if (const rosy::result res = engine.init(); res != rosy::result::ok) {
+		engine.deinit();
+		return 1;
+	}
+	if (const rosy::result res = engine.run(); res != rosy::result::ok) {
+		engine.deinit();
+		return 1;
+	}
+	engine.deinit();
 	return 0;
+}
+
+
+namespace rosy
+{
+	//// Engine
+	result engine::init()
+	{
+		std::cout << "Engine init!\n";
+		return result::ok;
+	}
+
+	result engine::run()
+	{
+		std::cout << "Engine run!\n";
+		return result::ok;
+	}
+
+	void engine::deinit()
+	{
+		std::cout << "Engine deinit!\n";
+	}
+
 }
--- Engine/Engine/Engine.h
+++ Engine/Engine/Engine.h
@@ -2,3 +2,20 @@
 #pragma warning(4)
 #pragma warning(error)
 #include <iostream>
+
+namespace rosy
+{
+
+	enum class result: uint8_t
+	{
+		ok,
+		error,
+	};
+
+	struct engine
+	{
+		result init();
+		result run();
+		void deinit();
+	};
+}
