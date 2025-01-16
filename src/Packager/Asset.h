2b04909a38629b3d3652147e72ce3a81b88fc6e9
Bjorn Tipling <bjorn.tipling@conductorone.com>
2025-01-15 18:27:22 -0800

    progress

---
+++ src/Packager/Asset.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <array>
+#include <format>
+#include <vector>
+#include "../Engine/Types.h"
+
+namespace rosy_packager {
+	struct position
+	{
+		std::array<float, 3> vertex{ 0.f, 0.f, 0.f };
+		std::array<float, 3> normal{ 0.f, 0.f, 0.f };
+	};
+
+	struct triangle
+	{
+		std::array<uint32_t, 3> indexes{ 0, 1, 2 };
+	};
+
+	struct asset
+	{
+		std::string path{"."};
+		std::vector<position> positions;
+		std::vector<triangle> triangles;
+
+		rosy::result write();
+		rosy::result read();
+	};
+}
--- src/Packager/Main.cpp
+++ src/Packager/Main.cpp
@@ -1,19 +1,15 @@
-#include <filesystem>
-#include <iostream>
+#include "Asset.h"
+
+using namespace rosy_packager;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-	int is_ok = EXIT_FAILURE;
-	FILE* stream;
-	std::cout << std::format("current file path: {}", std::filesystem::current_path().string()) << '\n';
-	errno_t err = fopen_s(&stream, "lol.txt", "w");
-	if (err != 0)
-	{
-		return is_ok;
+	asset a{};
+	if (const auto res = a.write(); res != rosy::result::ok) {
+		return EXIT_FAILURE;
 	}
-	const size_t res = fwrite("lol", 3, 1, stream);
-	std::cout << std::format("wrote {} elements", res) << '\n';
-	int num_closed = _fcloseall();
-	std::cout << std::format("closed {} files", num_closed) << '\n';
+	if (const auto res = a.read(); res != rosy::result::ok) {
+		return EXIT_FAILURE;
+	}
 	return 0;
 }
