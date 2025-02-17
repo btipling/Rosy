c06f9ff60291463f01a7251a6d58ccf65e1d9ba7
Bjorn Tipling <bjorn.tipling@conductorone.com>
2025-02-16 23:12:49 -0800

    improve build config

--- src/premake5.lua
+++ src/premake5.lua
@@ -1,82 +1,91 @@
 workspace "Rosy"
-   configurations { "Debug", "Release" }
-   warnings "Extra"
-   fatalwarnings "All"
+    configurations { "Debug", "Release", "RenderDoc", "Clang" }
+    filter "not configurations:Clang"
+        warnings "Extra"
+        fatalwarnings "All"
+    filter {}
 
 vk_sdk = os.getenv("VULKAN_SDK")
 
 project "Engine"
-   kind "ConsoleApp"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "bin/%{cfg.buildcfg}"
-   architecture("x86_64")
-   -- toolset("clang")
-   debugdir "./Engine/"
-   flags { "MultiProcessorCompile" }
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "bin/%{cfg.buildcfg}"
+    architecture("x86_64")
+    filter "configurations:Clang"
+        toolset("clang")
+    filter {}
+    debugdir "./Engine/"
+    flags { "MultiProcessorCompile" }
 
-   links { "SDL3" }
-   links { "flecs" }
-   links { "ktx" }
+    links { "SDL3" }
+    links { "flecs" }
+    links { "ktx" }
 
-   includedirs { "libs/SDL/include/" }
-   includedirs { vk_sdk .. "/Include/" }
-   includedirs { "libs/imgui/" }
-   includedirs { "libs/tracy/" }
-   includedirs { "libs/KTX-Software/include/" }
-   includedirs { "libs/flecs/include/" }
+    includedirs { "libs/SDL/include/" }
+    includedirs { vk_sdk .. "/Include/" }
+    includedirs { "libs/imgui/" }
+    includedirs { "libs/tracy/" }
+    includedirs { "libs/KTX-Software/include/" }
+    includedirs { "libs/flecs/include/" }
 
-   libdirs { "libs/SDL/build/Debug" }
-   libdirs { "libs/KTX-Software/build/Debug" }
-   libdirs { vk_sdk .. "/Lib/" }
-   libdirs { "libs/flecs/out/Debug" }
+    libdirs { "libs/SDL/build/Debug" }
+    libdirs { "libs/KTX-Software/build/Debug" }
+    libdirs { vk_sdk .. "/Lib/" }
+    libdirs { "libs/flecs/out/Debug" }
+    defines { "SIMDJSON_EXCEPTIONS=OFF" }
+    filter "configurations:RenderDoc"
+        defines { "RENDERDOC" }
+    filter {}
 
+    files { "Engine/**.h", "Engine/**.cpp" }
+    files { "Packager/Asset.h", "Packager/Asset.cpp" }
+    files { "libs/imgui/**.h", "libs/imgui/**.cpp" }
+    files { "libs/Volk.cpp" }
+    files { "libs/VMA.cpp" }
+    files { "shaders/*.slang", "shaders/*.ps1" }
+    
+    filter { "configurations:Debug or configurations:RenderDoc or configurations:Clang" }
+        defines { "DEBUG" }
+        symbols "On"
+    filter {}
 
-
-   files { "Engine/**.h", "Engine/**.cpp" }
-   files { "Packager/Asset.h", "Packager/Asset.cpp" }
-   files { "libs/imgui/**.h", "libs/imgui/**.cpp" }
-   files { "libs/Volk.cpp" }
-   files { "libs/VMA.cpp" }
-   files { "shaders/*.slang", "shaders/*.ps1" }
-
-   filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
-
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+    filter {}
 
     buildmessage "Compiling shaders?"
     prebuildcommands {
-        "/shaders/script_compile.ps1 ./shaders/"
+        "Powershell -File %{prj.location}/shaders/script_compile.ps1 ./shaders/"
     }
 
 project "Packager"
-   kind "ConsoleApp"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "bin/%{cfg.buildcfg}"
-   architecture("x86_64")
-   -- toolset("clang")
-   debugdir "./Packager/"
-   flags { "MultiProcessorCompile" }
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "bin/%{cfg.buildcfg}"
+    architecture("x86_64")
+    filter "configurations:Clang"
+        toolset("clang")
+    filter {}
+    debugdir "./Packager/"
+    flags { "MultiProcessorCompile" }
 
-   links { "fastgltf" }
+    links { "fastgltf" }
 
-   includedirs { "libs/fastgltf/include/" }
+    includedirs { "libs/fastgltf/include/" }
 
-   libdirs { "libs/fastgltf/build/Debug" }
+    libdirs { "libs/fastgltf/build/Debug" }
 
+    files { "Packager/**.h", "Packager/**.cpp" }
+    files { "Engine/Types.h", "Engine/Telemetry.h", "Engine/Telemetry.cpp" }
 
-   files { "Packager/**.h", "Packager/**.cpp" }
-   files { "Engine/Types.h", "Engine/Telemetry.h", "Engine/Telemetry.cpp" }
+    filter { "configurations:Debug or configurations:RenderDoc or configurations:Clang" }
+        defines { "DEBUG" }
+        symbols "On"
 
-   filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
-
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
