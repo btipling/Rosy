afed102d6d3e88f17a99ea1b63b495861bef2729
Bjorn Tipling <bjorn.tipling@conductorone.com>
2025-03-11 21:50:19 -0700

    ok this also builds

--- src/Packager/FBX.cpp
+++ src/Packager/FBX.cpp
@@ -318,7 +318,7 @@ namespace
                 // TODO: support child nodes.
                 //const size_t current_asset_node_index = fbx_asset.nodes.size();
                 node new_asset_node{};
-                //new_asset_node.name = std::vector<char>(node_name, node_name + strlen(node_name));
+                new_asset_node.name = std::vector(node_name, node_name + strlen(node_name));
                 new_asset_node.mesh_id = static_cast<uint32_t>(current_asset_mesh_index);
                 //fbx_asset.scenes[0].nodes.emplace_back(current_asset_node_index);
                 //fbx_asset.nodes.emplace_back(new_asset_node);
